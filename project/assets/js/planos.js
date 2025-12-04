7dd6105b0449a6f9a243874812f8e7a22865d26e
Psds13 <edmilsonsegundo15@gmail.com>
2025-12-04 20:27:21 -0300

    Arquivos atualizados

---
+++ project/assets/js/planos.js
@@ -0,0 +1,7 @@
+function assinar(plano) {
+    alert(`Você selecionou o plano: ${plano}. Em breve entraremos em contato!`);
+}
+
+function solicitarDemo() {
+    alert("Um consultor entrará em contato para agendar sua demonstração!");
+}
